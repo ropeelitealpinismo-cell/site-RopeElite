049091e5a85c26937d1e3fe3c6fb58d256b33e4f
ropeelitealpinismo-cell <ropeelite.alpinismo@gmail.com>
2025-11-20 15:44:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Substitua este link pelo seu link do Google Forms:
+let formURL = "https://docs.google.com/forms/d/e/1FAIpQLSckRB0BC4AS39mhuq3-8l21ePGm8Wqj_D6_ENFa66tVik3GRQ/viewform?usp=header";
+
+// Botões
+document.getElementById("orçamentoBtn").href = formURL;
+document.getElementById("formBtn").href = formURL;
